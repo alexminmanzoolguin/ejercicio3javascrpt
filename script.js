a5fd475b31b3e30e11591dc7c171d0dac4b8b80d
alex yazmin manzo olguin <ada_hack418@hackademy.mx>
2022-01-26 05:30:02 -0600

    solucion
    ejercicio

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("MANZO OLGUIN ALEX YAZMIN");
+
+const limnite=[50];
+function fibo(numero){
+let a=0,b=1,c,s=1;
+console.log(a);
+console.log(b);
+for(let i=0; i<=numero;i++){
+c=a+b;
+console.log(c);
+s=s+c;
+a=b;
+b=c;
+}
+}
+limnite.forEach(fibo);
+
+
